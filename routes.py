c767966d213cc612727297189b2d308df812d042
Lee Sharma <lee@leesharma.com>
2017-08-04 13:51:57 -0400

    Set up with Postgres

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
+import os
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL',
+                                                  'postgresql://localhost/learningflask')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 @app.route("/")
 def index():
